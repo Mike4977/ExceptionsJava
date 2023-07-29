6ee7aab5422cdabe45cf88f97ad9dc625816ab9a
Mike4977 <weilliss@mail.ru>
2023-07-29 14:43:25 +0600

    first commit

--- HW2/src/main/java/Task1.java
+++ HW2/src/main/java/Task1.java
@@ -1,19 +1,18 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        EnterFloatNumber();
+        inputNum();
     }
-
-    public static void EnterFloatNumber(){
+    public static void inputNum(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("введите дробное число \n");
-//        Float num = scanner.nextFloat();
-        if(!scanner.hasNextFloat()){
-            System.err.println("Ввели не дробное число! Попробуйте еще раз");
-//        }else {
-//            Float num = scanner.nextFloat();
-//            System.out.println(num);
+        try{
+            System.out.println(Float.parseFloat(scanner.next()));
+//            System.out.println(scanner.next());
+        }catch (RuntimeException e){
+            inputNum();
         }
     }
 }
